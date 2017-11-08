b8eb1205d7e67a0fff3882d463c9ae1069c7827c
Yanas <yanas@specialisterren.nl>
2017-11-08 16:23:00 +0100

    First commit

---
+++ src/main/java/Chat.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Chat {
+    private JPanel panelArea1;
+    private JTextArea msg_field;
+    private JButton button1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
